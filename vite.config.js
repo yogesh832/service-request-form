833802504217a86d23ed5602ce576cb26ee74329
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-06-18 22:23:00 +0530

    added basic npm

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit (in kB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react')) {
+            return 'react-vendor';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
