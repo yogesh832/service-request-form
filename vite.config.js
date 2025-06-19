dda4baf4f62301e60aae5d18c388c04fe8a8e47c
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-06-19 10:08:14 +0530

    sdfsdfdsf sdfsd

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // 👈 ensures assets use relative paths in production
   plugins: [react()],
+  base: '/', // DON'T use './' here – use root relative path
   build: {
+    outDir: 'dist',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules/react')) {
-            return 'react-vendor';
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
+          if (id.includes('node_modules/react')) return 'react-vendor';
+          if (id.includes('node_modules')) return 'vendor';
+        }
+      }
+    }
+  }
 });
